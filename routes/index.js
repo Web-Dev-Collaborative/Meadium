69e1297d762522370e2d6cfbb6e5e0ac38273898
nappernick <nickfmatthews@gmail.com>
2020-11-02 18:15:19 -0500

    Small updates

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const { User, Story } = require('../db/models')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   const stories = Story.findAll()
   res.render('index', {
     title: 'Meadium',
--- routes/login.js
+++ routes/login.js
@@ -2,16 +2,16 @@ const express = require('express');
 const loginRouter = express.Router();
 const { check } = require('express-validator');
 const bcrypt = require('bcryptjs');
-const { asyncHandler, handleValidationErrors, userValidations } = require('./utils.js')
+const { asyncHandler, handleValidationErrors } = require('./utils.js')
 const { loginUser } = require('../auth')
 
 const { User } = require('../db/models')
 
 loginRouter.get('/', (req, res) => {
-    res.render('logout')
+    res.render('login')
 })
 
-loginRouter.post('/', userValidations, handleValidationErrors, asyncHandler(async (req, res) => {
+loginRouter.post('/', handleValidationErrors, asyncHandler(async (req, res) => {
     const { usernameOrEmail, password } = req.body
     const hashedPassword = await bcrypt.hash(password, 10);
     let user
