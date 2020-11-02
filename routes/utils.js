3a5cb99577d510604bf2f2c531366dc248998fe2
IvanGent <gentilleivan@gmail.com>
2020-11-02 13:09:37 -0500

    added utils.js and auth.js

---
+++ routes/utils.js
@@ -0,0 +1,24 @@
+const csrf = require('csurf');
+const csrfProtection = csrf({cookie: true});
+
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+const handleValidationErrors = (req, res, next) => {
+    const validationErrors = validationResult(req);
+
+    if(!validationErrors.isEmpty()) {
+        const errors = validationErrors.array().map(error => error.msg);
+        const err = Error('Bad request.')
+        err.error = errors;
+        err.status = 404;
+        err.title = 'Bad request.'
+        next(err)
+    }
+    next();
+}
+
+module.exports = {
+    csrfProtection,
+    asyncHandler,
+    handleValidationErrors
+}
