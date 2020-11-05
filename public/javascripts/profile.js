bc68632617882ed8b70941f563ad713e3ae7306f
IvanGent <gentilleivan@gmail.com>
2020-11-05 18:25:39 -0500

    working on ajax for profile

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -11,7 +11,20 @@ window.addEventListener('load', () => {
         }
     })
 
-    searchInput.addEventListener('input', () => {
-        
+    searchInput.addEventListener('input', async (e) => {
+        const title = e.target.value;
+        const data = { title: title };
+        try {
+            const res = await fetch('http://localhost:8010/profile/search', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            });
+            const dataJSON = await res.json();
+            console.log(dataJSON);
+            console.log('hi from fetch')
+        } catch(e) {
+            console.log(e);
+        }
     })
 })
--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(express.json())
+router.use(bodyParser.json());
 
 const { User, Story, Pinned } = require('../db/models')
 const { asyncHandler } = require('./utils');
 const { requireAuth, logoutUser } = require('../auth');
 
 router.get('/', requireAuth, asyncHandler(async(req, res) => {
+    console.log('hi')
+    console.log(req.body);
     const id = res.locals.user.id
     const user = await User.findByPk(id);
     const authorId = user.id;
@@ -13,16 +18,26 @@ router.get('/', requireAuth, asyncHandler(async(req, res) => {
     ///////// Need to create Pinned table in database.
     // const pinnerId = user.id;
     // const pinned = await Pinned.findAll({where: { pinnerId }});
-    /////////
-    stories.forEach(story => {
-        console.log(story.id);
-    })
+    ///////
+    // res.json({ })
     res.render('profile', { user, stories });
 }));
 
+router.post('/search', asyncHandler(async(req, res) => {
+    console.log('hi')
+    const { title } = req.body;
+    const stories = await Story.findAll({
+        where: {
+            title: {
+                [Op.like]: `%${title}%`
+            }
+        }
+    });
+    return res.json({ stories });
+}));
+
 router.post('/logout', (req, res) => {
-    // logoutUser(req, res);
-    delete req.session.auth;
+    logoutUser(req, res);
     res.redirect('/');
 })
 
